1354643b7c58c1acd91ba61264712bd9f7866525
Luan Tenório <lluantenorio7@gmail.com>
2025-03-05 21:56:36 -0300

    Set routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToolBarComponent } from "./tool-bar/tool-bar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ToolBarComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'learn-web';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { SubjectComponent } from './subject/subject.component';
 
 export const routes: Routes = [
-    { path: "login", component: LoginComponent }
+    { path: "login", component: LoginComponent },
+    { path: "", component: SubjectComponent },
+    { path: "account", component: SubjectComponent },
+    { path: "timeline", component: SubjectComponent },
+    { path: "subjects", component: SubjectComponent },
+    { path: "reviews", component: SubjectComponent },
+    { path: "todos", component: SubjectComponent },
+    { path: "*", component: SubjectComponent }
 ];
